047b822556f871f2304a23a1158e86b9d87af600
ade.fathudin <ade.fathudin@bsminsbroker.com>
2019-09-29 05:44:15 +0700

    edit htaccess

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,11 +1,11 @@
 <?php
 defined('BASEPATH') OR exit ('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Dashboard extends CI_Controller {
     public function index()
     {
         $this->data['title'] = 'Data Log Aplikasi';
-        $this->data['subview'] = 'index';
+        $this->data['subview'] = 'dashboard/index';
         $this->load->view('_layout_main', $this->data);
     }
 
