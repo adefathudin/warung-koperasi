85511a7cb56a5048debe11d5f9ca115f23199528
ade.fathudin <ade.fathudin@bsminsbroker.com>
2019-09-29 10:42:38 +0700

    rev

--- application/views/login.php
+++ application/views/login.php
@@ -73,7 +73,7 @@
                     <a class="small" href="forgot-password.html">Forgot Password?</a>
                   </div>
                   <div class="text-center">
-                    <a class="small" href="register.html">Create an Account!</a>
+                    <a class="small" href="<?php echo base_url('auth/reg/')?>">Create an Account!</a>
                   </div>
                 </div>
               </div>
