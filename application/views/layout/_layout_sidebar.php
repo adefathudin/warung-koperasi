e95f011ffbd418a1982dca72e69c84d0671b1ec8
Ade Fathudin <adefathudin76@gmail.com>
2019-10-06 01:31:53 +0700

    revisi

--- application/views/layout/_layout_sidebar.php
+++ application/views/layout/_layout_sidebar.php
@@ -5,47 +5,55 @@
   <div class="sidebar-brand-icon rotate-n-15">
     <i class="fas fa-laugh-wink"></i>
   </div>
-  <div class="sidebar-brand-text mx-3">BAMISA <sup>2</sup></div>
+  <div class="sidebar-brand-text mx-3">WarKop Mas-Ja</div>
 </a>
 
 <!-- Divider -->
 <hr class="sidebar-divider my-0">
-
-<!-- Nav Item - Dashboard -->
+<!--
+ Nav Item - Dashboard 
 <li class="nav-item active">
   <a class="nav-link" href="index.html">
     <i class="fas fa-fw fa-tachometer-alt"></i>
     <span>Profil</span></a>
 </li>
-
+-->
 <!-- Divider -->
 <hr class="sidebar-divider">
-
-<!-- Heading -->
-<div class="sidebar-heading">
-  Rekeningku
-</div>
-
 <!-- Nav Item - Pages Collapse Menu -->
 <li class="nav-item">
-  <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+  <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseTwo">
     <i class="fas fa-fw fa-cog"></i>
-    <span>Tabungan</span>
+    <span>Rekeningku</span>
   </a>
-  <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+  <div id="collapseOne" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
     <div class="bg-white py-2 collapse-inner rounded">
-      <h6 class="collapse-header">Rekeningku:</h6>
       <a class="collapse-item" href="buttons.html">Transaksi</a>
       <a class="collapse-item" href="cards.html">Mutasi</a>
       <a class="collapse-item" href="cards.html">Pembukuan</a>
   </div>
 </li>
 
+<li class="nav-item">
+  <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+    <i class="fas fa-fw fa-cog"></i>
+    <span>Rekeningku</span>
+  </a>
+  <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+    <div class="bg-white py-2 collapse-inner rounded">
+      <a class="collapse-item" href="buttons.html">Transaksi</a>
+      <a class="collapse-item" href="cards.html">Mutasi</a>
+      <a class="collapse-item" href="cards.html">Pembukuan</a>
+  </div>
+</li>
+
+
 <!-- Nav Item - Utilities Collapse Menu -->
+
 <li class="nav-item">
   <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
     <i class="fas fa-fw fa-folder"></i>
-    <span>Bakti Sosial</span>
+    <span>Simpan</span>
   </a>
   <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
     <div class="bg-white py-2 collapse-inner rounded">
